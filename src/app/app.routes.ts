07d10e82124ee0cba7a544f2374465b024074206
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-06-04 12:28:37 +0800

    added charts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/bar-chart',
+        pathMatch: 'full'
+    },
+    {
+        path: 'bar-chart', 
+        component: BarChartComponent,
+        title: 'Bar Chart'
+    },
+    {
+        path: 'line-chart', 
+        component: LineChartComponent,
+        title: 'Line Chart'
+    },
+];
